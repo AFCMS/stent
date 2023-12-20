32b22e4ade0845cdae2d5a194392c8cd0d52cafb
AFCMS <afcm.contact@gmail.com>
2023-12-21 00:30:57 +0100

    Add LuaLS typing annotations + fix formatting stuff

--- init.lua
+++ init.lua
@@ -1,27 +1,46 @@
 stent = {}
-stent.start_location = vector.new(0,0,0) -- to override in mods.
---  Players are placed here when they join.
-stent.place_spawn_schematic = function() end
 
+---Players are placed here when they join.
+---
+---Should be overriden by mods
+stent.start_location = vector.new(0, 0, 0)
+
+function stent.place_spawn_schematic() end
 
 local storage = minetest.get_mod_storage()
 
--- gets next unique (to the world) id and makes sure it will never be returned again.
-local get_next_uid = function ()
+---Gets next unique (to the world) id and makes sure it will never be returned again.
+---@return integer
+local function get_next_uid()
     local uid = storage:get_int("id") or 0
-    storage:set_int("id",uid+1)
+    storage:set_int("id", uid + 1)
     return uid
 end
 
-
+---@param mod_name string
+---@param arena_name string
 function stent.create_arena(mod_name, arena_name)
     arena_lib.create_arena("", mod_name, arena_name)
     arena_lib.set_entrance_type("", mod_name, arena_name, "formspec_entrace")
     arena_lib.set_entrance("", mod_name, arena_name, "add")
 end
 
+---@class stent.arena_props
+---@field enabled boolean?
+---@field pos1 mt.Vector?
+---@field pos2 mt.Vector?
+---@field min_players integer?
+---@field max_players integer?
+---@field weather_condition any?
+---@field lighting any?
+---@field [string] any
+
+---@param mod_name string
+---@param arena_name string
+---@param props stent.arena_props
 function stent.set_arena_props(mod_name, arena_name, props)
-    local arena_id, arena = arena_lib.get_arena_by_name(mod_name,arena_name)
+    local arena_id, arena = arena_lib.get_arena_by_name(mod_name, arena_name)
+
     if arena.enabled then
         arena_lib.disable_arena("", mod_name, arena_name)
     end
@@ -39,21 +58,19 @@ function stent.set_arena_props(mod_name, arena_name, props)
     end
 
     -- set arena properties:
-    for prop_name,val in pairs(props) do
-        if prop_name ~= "lighting" and 
-            prop_name ~= "weather_condition" and 
-            prop_name ~= "pos1" and 
-            prop_name ~= "pos2" and 
-            prop_name ~= "min_players" and 
+    for prop_name, val in pairs(props) do
+        if prop_name ~= "lighting" and
+            prop_name ~= "weather_condition" and
+            prop_name ~= "pos1" and
+            prop_name ~= "pos2" and
+            prop_name ~= "min_players" and
             prop_name ~= "max_players" then
-
-            arena_lib.change_arena_property("",  mod_name, arena_name, prop_name, val)
+            arena_lib.change_arena_property("", mod_name, arena_name, prop_name, val)
         end
     end
     arena_lib.mods[mod_name].settings.return_point = stent.start_location
     arena_lib.enable_arena("", mod_name, arena_name, false)
 end
-
 
 -- Arena lib entrance registration
 
@@ -69,7 +86,7 @@ arena_lib.register_entrance_type("stent", "formspec_entrace", {
     -- to run entrance-specific checks and return the value that arena_lib will
     -- then store as an entrance
 
-    on_add = function (sender, mod, arena)
+    on_add = function(sender, mod, arena)
         local uid = get_next_uid()
         return uid
     end,
@@ -79,22 +96,22 @@ arena_lib.register_entrance_type("stent", "formspec_entrace", {
     -- exist) and then remove the entrance. Use this callback just to run
     -- entrance-specific checks.
 
-    on_remove = function (mod, arena)
+    on_remove = function(mod, arena)
         return
     end,
-    
+
     -- what should happen to each entrance when the status of the associated
     -- arena changes (e.g. when someone enters, when the arena gets disabled
     -- etc.)
 
-    on_update =function (mod, arena)
+    on_update = function(mod, arena)
         -- TODO: send updated formspecs
         stent.refresh_formspecs()
     end,
 
     -- additional actions to perform when the server starts. Useful for nodes,
     -- since they don't have an on_activate callback, contrary to entities
-    on_load = function (mod, arena)
+    on_load = function(mod, arena)
         -- stent.on_load()
         -- stent.refresh_formspecs()
     end,
@@ -115,25 +132,42 @@ arena_lib.register_entrance_type("stent", "formspec_entrace", {
         -- called when entering the editor. Useful to reset entrance properties
         -- bound to p_name, as it's the only way the player has to know that the
         -- editor has been entered by someone
-        on_enter = function (p_name, mod, arena)
+        on_enter = function(p_name, mod, arena)
             return
         end,
     },
     -- what the debug log should print (via arena_lib.print_arena_info())
-    debug_output = function (entrace)
+    debug_output = function(entrace)
         return "debug"
     end,
 })
 
+---@class stent.arena_data
+---@field name string
+---@field mod string
+---@field icon string
+---@field players_inside integer
+---@field max_players integer
+---@field in_queue boolean
+---@field in_loading boolean
+---@field in_game boolean
+---@field in_celebration boolean
+---@field enabled boolean
 
+---@type stent.arena_data[]
 stent.saved_arenas_data = {}
 
-function stent.build_mainmenu_formspec(p_name, arenas_data) 
+---Build the formspec displayed to the player
+---
+---Should be overriden by mods
+---@param p_name string
+---@param arenas_data stent.arena_data[]
+---@return string
+function stent.build_mainmenu_formspec(p_name, arenas_data)
     return ""
 end
 
-stent.refresh_formspecs = function ()
-
+function stent.refresh_formspecs()
     -- first, get the data needed to refresh the formspecs
 
     -- arena_lib.mods[yourmod].arenas
@@ -164,14 +198,13 @@ stent.refresh_formspecs = function ()
     -- next, call the function to build the formspec, passing the data
     for _, player in pairs(minetest.get_connected_players()) do
         local p_name = player:get_player_name()
-        if not(arena_lib.is_player_in_arena(p_name)) then
+        if not (arena_lib.is_player_in_arena(p_name)) then
             local fs = stent.build_mainmenu_formspec(p_name, arenas_data)
             player:set_inventory_formspec(fs)
-            minetest.show_formspec(p_name,"",fs)
+            minetest.show_formspec(p_name, "", fs)
         end
     end
 end
-
 
 -- we will set the inventory formspec to be the main menu, and automatically
 -- open it. We should save the old inventory formspecs and renew them before
@@ -180,17 +213,19 @@ end
 -- minigame, we close the main menu and reset their inventory to the default.
 local old_inventory_formspecs = {}
 
+---@param p_name string
 local function set_main_menu(p_name)
     local player = minetest.get_player_by_name(p_name)
     old_inventory_formspecs[p_name] = player:get_inventory_formspec()
-    if player then 
-        local fs = stent.build_mainmenu_formspec(p_name,stent.saved_arenas_data)
+    if player then
+        local fs = stent.build_mainmenu_formspec(p_name, stent.saved_arenas_data)
         player:set_inventory_formspec(fs)
         minetest.show_formspec(p_name, "", fs)
     end
 end
 
-local function unset_main_menu(p_name) 
+---@param p_name string
+local function unset_main_menu(p_name)
     minetest.close_formspec(p_name, "")
     local player = minetest.get_player_by_name(p_name)
     if player then
@@ -204,19 +239,19 @@ arena_lib.register_on_load(function(mod, arena)
     end
 end)
 
-arena_lib.register_on_end(function(mod, arena, winners, is_forced) 
+arena_lib.register_on_end(function(mod, arena, winners, is_forced)
     for p_name, data in pairs(arena.players) do
         set_main_menu(p_name)
     end
 end)
 
-arena_lib.register_on_quit(function(mod, arena, p_name, is_spectator, reason) 
+arena_lib.register_on_quit(function(mod, arena, p_name, is_spectator, reason)
     set_main_menu(p_name)
 end)
 
 
 minetest.register_on_joinplayer(function(player, last_login)
-    local p_name = player:get_player_name() 
+    local p_name = player:get_player_name()
     player:set_pos(stent.start_location)
     set_main_menu(p_name)
 end)
